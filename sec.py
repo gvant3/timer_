476fe9e991f793b563fa512ab4fe69a5ef18d5e5
gvant3 <gvantolo@gmail.com>
2025-04-23 20:53:32 +0200

    Add files via upload

--- sec.py
+++ sec.py
@@ -38,13 +38,38 @@ def delete_sec():
     lb_sec.setText(str(sec))
 rebtn.clicked.connect(delete_sec)
 
+sec2 = 0
+
 win_t = QWidget()
 win_t.setWindowTitle("Таймер")
-win_t.resize(200, 200)
+win_t.resize(400, 200)
 layo_tim1 = QVBoxLayout()
 win_t.setLayout(layo_tim1)
 secbtn = QPushButton("Секундомер")
 layo_tim1.addWidget(secbtn)
+stmbtn = QPushButton("Старт")
+layo_tim1.addWidget(stmbtn)
+sttmbtn = QPushButton("пауза")
+layo_tim1.addWidget(sttmbtn)
+resbtn = QPushButton("Обновити")
+layo_tim1.addWidget(resbtn)
+addsec = QPushButton("Добавитити секунду")
+layo_tim1.addWidget(addsec)
+addmin = QPushButton("Добавити хвилину")
+layo_tim1.addWidget(addmin)
+minussec = QPushButton("відняти секунду")
+layo_tim1.addWidget(minussec)
+minmin = QPushButton("відняти хвилину")
+layo_tim1.addWidget(minmin)
+layo_sec1 = QVBoxLayout()
+lb_sec1 = QLabel("0")
+layo_tim1.addWidget(lb_sec1)
+
+
+
+
+
+
 def tim_show():
     time.stop()
     win_t.show()
@@ -54,6 +79,66 @@ def tim_hide():
     win_t.hide()
     win_s.show()
 secbtn.clicked.connect(tim_hide)
+
+def add_sec():
+    global sec2
+    sec2 += 1
+    lb_sec1.setText(str(sec2))
+addsec.clicked.connect(add_sec)
+def minus_sec():    
+    global sec2
+    sec2 -= 1
+    if sec2 <= 0:
+        sec2 = 0
+    lb_sec1.setText(str(sec2))   
+minussec.clicked.connect(minus_sec)
+def update_tim():
+    global sec2
+    sec2 -= 1
+    lb_sec1.setText(str(sec2))
+    if sec2 <= 0:
+        sec2 = 0
+        time.stop()
+time.timeout.connect(update_tim)
+def start_tim():
+    time.start(1000)
+stmbtn.clicked.connect(start_tim)
+def stop_tim():
+    time.stop() 
+sttmbtn.clicked.connect(stop_tim)
+
+def reset_tim():
+    global sec2
+    sec2 = 0
+    lb_sec1.setText(str(sec2))
+    time.stop()
+resbtn.clicked.connect(reset_tim)
+
+def add_min():
+    global sec2
+    sec2 += 60
+    lb_sec1.setText(str(sec2))
+addmin.clicked.connect(add_min)
+
+def minus_min():
+    global sec2
+    sec2 -= 60
+    if sec2 <= 0:
+        sec2 = 0
+    lb_sec1.setText(str(sec2))
+minmin.clicked.connect(minus_min)
+
+
+
+
+
+
+
+
+
+
+win_s.show()
+app.exec_()  
 
 
 
